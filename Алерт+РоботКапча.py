76254fa96850cb0844743a9aca3d66972649dcef
AntonBadanin <52920212+AntonBadanin@users.noreply.github.com>
2019-09-19 18:18:02 +0300

    add StepikTask №2.6

---
+++ Алерт+РоботКапча.py
@@ -0,0 +1,30 @@
+from selenium import webdriver
+import time
+import math
+
+def calc(x):
+  return str(math.log(abs(12*math.sin(int(x)))))
+link = "http://suninjuly.github.io/alert_accept.html"
+
+try:
+    browser = webdriver.Chrome()
+    browser.get(link)
+    button = browser.find_element_by_css_selector("button.btn")
+    button.click()
+    confirm = browser.switch_to.alert # переключаемся на алерт
+    confirm.accept() # жмем согласится
+    x_element = browser.find_element_by_id("input_value")
+    x = x_element.text
+    y = calc(x)
+    input1 = browser.find_element_by_id("answer")
+    input1.send_keys(y)
+    button1 = browser.find_element_by_css_selector("button.btn")
+    button1.click()
+    
+finally:
+    # успеваем скопировать код за 30 секунд
+    time.sleep(10)
+    # закрываем браузер после всех манипуляций
+    browser.quit()
+
+# не забываем оставить пустую строку в конце файла
