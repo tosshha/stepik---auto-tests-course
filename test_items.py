144b9100830a4e83b07e18facb07dd35259e0ee5
tosshha <38882612+tosshha@users.noreply.github.com>
2019-09-25 19:50:04 +0300

    Update test_items.py

--- test_items.py
+++ test_items.py
@@ -2,7 +2,7 @@ import time
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 
-def test_guest_should_see_login_link(browser):
+def test_backet_verivication(browser):
     browser.get(link)
     assert browser.find_element_by_class_name("btn.btn-lg.btn-primary.btn-add-to-basket"),"Где кнопка то? Нету кнопки"
     #time.sleep(30)
